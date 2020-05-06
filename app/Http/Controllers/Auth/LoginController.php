e54b1d844d389aaf602bf7b995a6e11c249ab1f0
Goran Radmanovic <goranradmanovic@gmail.com>
2020-05-06 16:10:40 +0200

    Creating login and logout function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,31 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+          'email' => 'required|email|min:5',
+          'password' => 'required|min:5'
+        ]);
+
+        if(!auth()->attempt($request->only('email', 'password')))
+        {
+          return response(['logged' => false], 200)->header('Content-Type', 'application/json');
+        }
+
+        $user = auth()->user();
+
+        auth()->login($user);
+
+        return response(['logged' => true], 200)->header('Content-Type', 'application/json');
+    }
+
+    public function logout()
+    {
+      if (auth()->logout() === null) {
+        return response(['loggedout' => true], 200)->header('Content-Type', 'application/json');
+      }
     }
 }
