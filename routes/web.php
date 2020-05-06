7ef87dea2bb5ea4845345411d346424bbdebe787
Goran Radmanovic <goranradmanovic@gmail.com>
2020-05-06 18:54:23 +0200

    Uncommenting route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::auth();
 
+Route::get('/', 'WelcomeController@index')->name('welcome');
 Route::get('/home', 'HomeController@index')->name('home');
